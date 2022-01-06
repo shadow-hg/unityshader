111633c0e9584d8f2797db20049b74c925cb67d4
zhanghg <zhanghgyoozoo.com>
2022-01-06 18:50:43 +0800

    升级脚本功能
    1、可以通过面板实时修改矩阵的参数并重新生成 2、可以自定义x合z轴的数量，y轴的数量会自动计算

--- Assets/Scripts/InstanceEffect.cs
+++ Assets/Scripts/InstanceEffect.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class InstanceEffect : MonoBehaviour
 {
+    public bool reset = false;
+    [Space(25)]
     public List<GameObject> effectsPrefabs;
 
     [Header("生成物体的总数：")] public int effectCount = 1; //生成的数量
@@ -13,29 +15,36 @@ public class InstanceEffect : MonoBehaviour
     [Header("每个物体的间隔：")] public Vector3 interval = new Vector3(1, 1, 1); //每个特效的间隔
     [Header("开始生成的原点：")] public Vector3 initialPos = new Vector3(1, 1, 1); //生成特效的原点
 
-    [Header("每行数量：")] public int nn = 0;
+    [Header("每行数量：")] public Vector3 nn = new Vector3(1,1,1);
 
     private int _mm = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        GenerateEff(effectCount, interval);
+        //GenerateEff(effectCount, interval);
     }
 
 
-    /*void ThreeRoot(int a)
+    private void Update()
     {
-        while (_mm*_mm*_mm <= a)
+        if (reset != false)
         {
-            _nn = Mathf.Abs(_mm * _mm * _mm - a); 
-            _mm++;
+            for (int i = 0; i < this.transform.childCount; i++)
+            {
+                Destroy(this.transform.GetChild(i).gameObject);
+            }
+
+            _mm = 0;
+            GenerateEff(effectCount, interval);
+            reset = false;
         }
-    }*/
+    }
 
 
     public void GenerateEff(int eC, Vector3 iE)
     {
+        nn.z = effectCount / (nn.y * nn.x) + effectCount % (nn.y * nn.x);
         if (eC <= effectsPrefabs.Count)
         {
             for (int i = 0; i <= eC - 1; i++)
@@ -48,13 +57,13 @@ public class InstanceEffect : MonoBehaviour
         {
             Vector3 wldPos = new Vector3(0, 0, 0);
 
-            for (int i = 0; i < nn; i++)
+            for (int i = 0; i < nn.y; i++)
             {
                 wldPos.y = i * interval.y;
-                for (int j = 0; j < nn; j++)
+                for (int j = 0; j < nn.x; j++)
                 {
                     wldPos.x = j * interval.x;
-                    for (int k = 0; k < nn; k++)
+                    for (int k = 0; k < nn.z; k++)
                     {
                         wldPos.z = k * interval.z;
                         int kk = _mm % effectsPrefabs.Count;

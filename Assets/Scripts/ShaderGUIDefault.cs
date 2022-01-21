9afde6ca9bfd094bc8b8799dbafeeea7f78bf56f
zhanghg <zhanghgyoozoo.com>
2022-01-21 18:51:39 +0800

    dev: 添加自定义ShaderGUI和默认UIshader

---
+++ Assets/Scripts/ShaderGUIDefault.cs
@@ -0,0 +1,250 @@
+﻿using Gameplay.PVE.Skill;
+using RuntimeInspectorNamespace;
+using UnityEditor;
+using UnityEngine;
+
+/* 通用CustomShaderGUI:
+ * 
+ * shader需要符合以下属性命名规范，以下GUI才会生效
+ *
+ * 颜色：_Color
+ * 亮度：_ColorStrength
+ * 主贴图: _MainTex
+ * 遮罩：_MaskTex
+ * 指数：_Pow
+ * 深度偏移：_DepthOffset
+ * 是否使用mask贴图：[Toggle]_UseMask
+ * 主贴图是否开启UV动画：_MainAnimation
+ * mask贴图是否开启UV动画：_MaskAnimation
+ * 混合模式：_SrcAlpha
+ *         _DstAlpha
+ * 模板测试：
+ *      _Stencil
+ *      _StencilComp
+ *      _StencilOp
+ * 剔除：_Cull
+ * 
+ */
+
+public class ShaderGUIDefault : ShaderGUI
+{
+    public MaterialEditor materialEditor;
+    public MaterialProperty[] properties;
+    public Material targetMat;
+
+    private bool useMaskBool = false;
+    bool maskBool = false;
+    bool animBool = false;
+    bool steniBool = false;
+    bool blendBool = false;
+
+    public bool showHelp = false;
+
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        this.materialEditor = materialEditor;
+        this.properties = properties;
+        targetMat = materialEditor.target as Material;
+        ShowTex();
+        ShowMask();
+        ShowColorSetting();
+        ShowBlendMode();
+        ShowCull();
+        ShowAnim();
+        ShowStencil();
+        materialEditor.RenderQueueField();
+        ShowHelp();
+    }
+
+    void ShowColorSetting()
+    {
+        if (targetMat.HasProperty("_Color"))
+        {
+            MaterialProperty color = FindProperty("_Color", properties);
+            materialEditor.ShaderProperty(color, "Color：");
+        }
+
+        if (targetMat.HasProperty("_ColorStrength"))
+        {
+            MaterialProperty colorStr = FindProperty("_ColorStrength", properties);
+            materialEditor.ShaderProperty(colorStr, "亮度：");
+        }
+
+        if (targetMat.HasProperty("_Pow"))
+        {
+            MaterialProperty pow = FindProperty("_Pow", properties);
+            materialEditor.ShaderProperty(pow, "曝光：");
+        }
+
+        if (targetMat.HasProperty("_DepthOffset"))
+        {
+            MaterialProperty depthOffset = FindProperty("_DepthOffset", properties);
+            materialEditor.ShaderProperty(depthOffset, "深度偏移：");
+        }
+    }
+
+    void ShowTex()
+    {
+        MaterialProperty mainTex = FindProperty("_MainTex", properties);
+        materialEditor.ShaderProperty(mainTex, "MainTex");
+    }
+
+    void ShowMask()
+    {
+        if (targetMat.HasProperty("_UseMask")) //如果shader里有是否使用mask的toggle则显示这个toggle
+        {
+            MaterialProperty useMask = FindProperty("_UseMask", properties);
+            materialEditor.ShaderProperty(useMask, "Mask贴图：");
+            if (useMask.floatValue == 1)
+            {
+                useMaskBool = true;
+
+                if (targetMat.HasProperty("_MaskTex"))
+                {
+                    MaterialProperty maskTex = FindProperty("_MaskTex", properties);
+                    materialEditor.ShaderProperty(maskTex, "MaskTex");
+                }
+                else
+                {
+                    EditorGUILayout.LabelField("本Shader没有MaskTex功能，请使用UIDefault Shader");
+                }
+            }
+            else
+            {
+                useMaskBool = false;
+            }
+        }
+        else //如果shader里没有使用mask的toggle则不显示这个toggle
+        {
+            useMaskBool = false;
+
+            if (targetMat.HasProperty("_MaskTex"))
+            {
+                MaterialProperty maskTex = FindProperty("_MaskTex", properties);
+                materialEditor.ShaderProperty(maskTex, "MaskTex");
+            }
+        }
+    }
+
+    void ShowAnim()
+    {
+        animBool = EditorGUILayout.BeginFoldoutHeaderGroup(animBool, "UV动画");
+        if (animBool)
+        {
+            EditorGUILayout.LabelField("(值越大速度越快)");
+
+            if (targetMat.HasProperty("_MainAnimation"))
+            {
+                MaterialProperty mainAnimation = FindProperty("_MainAnimation", properties);
+                materialEditor.ShaderProperty(mainAnimation, "MainSpeed");
+                if (mainAnimation.floatValue == 1)
+                {
+                    MaterialProperty MainSpeedU = FindProperty("_MainSpeedU", properties);
+
+                    materialEditor.ShaderProperty(MainSpeedU, "主图X轴位移：");
+                    MaterialProperty MainSpeedV = FindProperty("_MainSpeedV", properties);
+                    materialEditor.ShaderProperty(MainSpeedV, "主图X轴位移：");
+                }
+            }
+
+            if (useMaskBool)
+            {
+                if (targetMat.HasProperty("_MaskAnimation"))
+                {
+                    MaterialProperty maskAnimation = FindProperty("_MaskAnimation", properties);
+                    materialEditor.ShaderProperty(maskAnimation, "MaskSpeed");
+                    if (maskAnimation.floatValue == 1)
+                    {
+                        MaterialProperty MaskSpeedU = FindProperty("_MaskSpeedU", properties);
+
+                        materialEditor.ShaderProperty(MaskSpeedU, "Mask图X轴位移：");
+                        MaterialProperty MaskSpeedV = FindProperty("_MaskSpeedV", properties);
+                        materialEditor.ShaderProperty(MaskSpeedV, "Mask图X轴位移：");
+                    }
+                }
+            }
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
+    }
+
+    void ShowBlendMode()
+    {
+        if (targetMat.HasProperty("_BlendModeEnum"))
+        {
+            MaterialProperty blendmodeEnum = FindProperty("_BlendModeEnum", properties);
+            materialEditor.ShaderProperty(blendmodeEnum, "混合模式");
+            switch (blendmodeEnum.floatValue)
+            {
+                case 0:
+                    targetMat.SetFloat("_SrcAlpha", 5);
+                    targetMat.SetFloat("_DstAlpha", 10);
+                    break;
+                case 1:
+                    targetMat.SetFloat("_SrcAlpha", 1);
+                    targetMat.SetFloat("_DstAlpha", 1);
+                    break;
+                case 2:
+                    targetMat.SetFloat("_SrcAlpha", 2);
+                    targetMat.SetFloat("_DstAlpha", 0);
+                    break;
+                case 3:
+                    targetMat.SetFloat("_SrcAlpha", 1);
+                    targetMat.SetFloat("_DstAlpha", 4);
+                    break;
+                case 4:
+                    targetMat.SetFloat("_SrcAlpha", 2);
+                    targetMat.SetFloat("_DstAlpha", 3);
+                    break;
+            }
+        }
+    }
+
+    void ShowStencil()
+    {
+        if (targetMat.HasProperty("_Stencil"))
+        {
+            steniBool = EditorGUILayout.BeginFoldoutHeaderGroup(steniBool, "模板测试");
+            if (steniBool)
+            {
+            
+                MaterialProperty stencilID = FindProperty("_Stencil", properties);
+                materialEditor.ShaderProperty(stencilID, "Stencil ID");
+
+                MaterialProperty stencilComp = FindProperty("_StencilComp", properties);
+                materialEditor.ShaderProperty(stencilComp, "Stencil Comparison");
+                MaterialProperty stencilOp = FindProperty("_StencilOp", properties);
+                materialEditor.ShaderProperty(stencilOp, "Stencil Operation");
+            
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
+    }
+
+    void ShowCull()
+    {
+        if (targetMat.HasProperty("_Cull"))
+        {
+            MaterialProperty cull = FindProperty("_Cull", properties);
+            materialEditor.ShaderProperty(cull, "选择剔除");
+        }
+    }
+
+    void ShowHelp()
+    {
+        EditorGUILayout.LabelField("---------------------------------------------------------------------------------------------------------------------");
+        showHelp = EditorGUILayout.BeginFoldoutHeaderGroup(showHelp, "查看帮助");
+        if (showHelp)
+        {
+            EditorGUILayout.LabelField("一些特殊参数的作用：");
+            EditorGUILayout.LabelField("曝光：默认值为1；0-1时暗部越亮；大于1时亮的地方越亮，暗的越暗.");
+            EditorGUILayout.LabelField("深度偏移：默认值为0；小于0时显示在物体前面；大于1时会被物体遮挡；可用于特效与地面的接缝.");
+            EditorGUILayout.LabelField("选择剔除：默认值为Back（剔除模型背面不渲染）；当特效或模型的内面永远不会被摄像机拍到时选择Back.");
+            if (targetMat.HasProperty("_Stencil"))
+            {
+                EditorGUILayout.LabelField("模板测试：默认值为0,Always,Keep可用于UI的裁剪.");
+            }
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+    }
+}
---
+++ Assets/Shader/自定义默认UIShader/UIDefault.shader
@@ -0,0 +1,171 @@
+﻿Shader "HRP/GGame/UI/UIDefault"
+{
+    Properties
+    {
+
+        _MainTex("MainTex", 2D) = "white" {}
+
+        [Toggle]_UseMask("UseMask",float) = 0
+        _MaskTex("MaskTex", 2D) = "white" {}
+
+        _Color("Color",Color) = (0.5,0.5,0.5,0.5)
+        _ColorStrength("ColorStrength",float) = 1.0
+        _Pow("Pow",float) = 1.0
+        _DepthOffset("DepthOffset",float) = 0.0
+
+        [Toggle]_MainAnimation("MainAnimation",float) = 0
+        _MainSpeedU("MainSpeedU", Float) = 0
+        _MainSpeedV("MainSpeedV", Float) = 0
+        [Toggle]_MaskAnimation("MaskAnimation",float) = 0
+        _MaskSpeedU("MaskSpeedU", Float) = 0
+        _MaskSpeedV("MaskSpeedV", Float) = 0
+
+        [Enum(Default,0,Additive,1,Multiply,2,SoftAdditive,3,2xMultiply,4)]
+        _BlendModeEnum("_BlendModeEnum",float) = 0
+        _SrcAlpha("SrcAlpha",float) = 5 //5,4,2,1 /5-10,4-1,2-0,1-1,6-1
+        _DstAlpha("DstAlpha",float) = 10 //10,1,0,6
+        
+        _Stencil("Stencil ID", Float) = 0
+        [Enum(UnityEngine.Rendering.CompareFunction)]_StencilComp("Stencil Comparison", Float) = 8
+        [Enum(UnityEngine.Rendering.StencilOp)]_StencilOp("Stencil Operation", Float) = 0
+        //_StencilWriteMask("Stencil Write Mask", Float) = 255
+        //_StencilReadMask("Stencil Read Mask", Float) = 255
+
+        [Enum(UnityEngine.Rendering.CullMode)]_Cull("Cull",float) = 2
+
+        //[Enum(UnityEngine.Rendering.CompareFunction)]_ZTest("ZTest", Float) = 4
+
+    }
+    SubShader
+    {
+        Tags
+        {
+            "LightMode" = "HRPForward"
+            "IgnoreProjector" = "True"
+            "Queue" = "Transparent"
+            "RenderType" = "Transparent"
+        }
+        Stencil
+        {
+            Ref[_Stencil]
+            Comp[_StencilComp]
+            Pass[_StencilOp]
+            //ReadMask[_StencilReadMask]
+            //WriteMask[_StencilWriteMask]
+        }
+        Pass
+        {
+
+            Blend [_SrcAlpha] [_DstAlpha]
+            Cull [_Cull]
+            //ZTest[_ZTest]
+            ZWrite off
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma shader_feature _PACKUV
+            #pragma shader_feature _USEMASK_ON
+            #pragma shader_feature _MAINANIMATION_ON
+            #pragma shader_feature _MASKANIMATION_ON
+
+            #include "Packages/com.yoozoo.owl.rendering.hrp/RenderPipeline/ShaderLibrary/HPipelineData.hlsl"
+            //#pragma only_renderers d3d9 d3d11 glcore gles gles3 metal
+            #include "GTA_H_FX_Common.hlsl"
+
+            CBUFFER_START(UnityPerMaterial)
+            half _ColorStrength;
+            half _Pow;
+            half _TimeModY;
+            half _TimeCycle;
+            half _TimeOffset;
+            CBUFFER_END
+
+            struct VertexInput
+            {
+                float4 vertex : POSITION;
+                float4 texcoord0 : TEXCOORD0;
+
+                #if defined(_USEMASK_ON)
+                float4 texcoord1 : TEXCOORD1;
+                #endif
+
+                half4 vertexColor : COLOR;
+            };
+
+            struct VertexOutput
+            {
+                float4 pos : SV_POSITION;
+                
+                VERTEX_INPUT_PACK_UV(0)
+
+                #if defined(_USEMASK_ON)
+                VERTEX_INPUT_PACK_UV(1)
+                float4 movingUV :TEXCOORD2;
+                #else
+                float2 movingUV :TEXCOORD2;
+                #endif
+
+                half4 color:TEXCOORD3;
+            };
+
+            VertexOutput vert(VertexInput v)
+            {
+                VertexOutput o;
+                o.pos = TransformObjectToHClip(v.vertex);
+
+                SETUP_PACK_UV(0, 0)
+
+                #if defined(_USEMASK_ON)
+                SETUP_PACK_UV(1, 1)
+                #endif
+
+                o.pos = vertDepthOffset(o.pos, _DepthOffset);
+                
+                o.movingUV.xy = v.texcoord0.xy;
+                #if defined (_MAINANIMATION_ON)
+                o.movingUV.xy = half2(_MainSpeedU, _MainSpeedV) * _Time.g + v.texcoord0.xy;
+                #endif
+                o.movingUV.xy = TRANSFORM_TEX(o.movingUV.xy, _MainTex);
+
+                #if defined (_USEMASK_ON)
+                o.movingUV.zw = v.texcoord1.xy;
+                #if defined (_MASKANIMATION_ON)
+                o.movingUV.zw = half2(_MaskSpeedU, _MaskSpeedV) * _Time.g + v.texcoord1.xy;
+                #endif
+                o.movingUV.zw = TRANSFORM_TEX(o.movingUV.zw, _MaskTex);
+                #endif
+
+                o.color = v.vertexColor * _Color;
+                return o;
+            }
+
+            half4 frag(VertexOutput i) : SV_Target
+            {
+                float2 uvMain = i.movingUV.xy;
+                APPLY_PACK_UV(uvMain, 0)
+
+                #if defined (_USEMASK_ON)
+                float2 uvMask = i.movingUV.zw;
+                APPLY_PACK_UV(uvMask, 1)
+                half4 _MaskTex_var = tex2D(_MaskTex, uvMask);
+                #endif
+                
+                half4 _MainTexVar = tex2D(_MainTex, uvMain);
+
+                half3 finalColor = _MainTexVar.rgb * i.color.rgb;
+                finalColor = saturate(pow(finalColor * _ColorStrength, _Pow));
+                
+                #if defined(_USEMASK_ON)
+                half finalAlpha = _MainTexVar.a * i.color.a * _MaskTex_var.r;
+                #else
+                half finalAlpha = _MainTexVar.a * i.color.a;
+                #endif
+
+                return half4(finalColor, finalAlpha);
+            }
+            ENDHLSL
+        }
+    }
+    CustomEditor"ShaderGUIDefault"
+}
